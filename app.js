d7e3ab3e006c50634f3f8c42475fa66d8b58940e
Kelish Rai <raikelish7@gmail.com>
2023-12-01 19:24:58 +0545

    added john smilga's repo

--- app.js
+++ app.js
@@ -1,18 +1,11 @@
-const { readFile,writeFile } = require('fs');
-const util = require('util');
-const uRF = util.promisify(readFile);
-const wRF = util.promisify(writeFile);
+const http = require('http');
+const fs = require('fs');
 
-const start = async() => {
-    try {
-        const value1 = await uRF('forRead.txt', 'utf-8');
-        const value2 = await uRF('for2ndRead.txt', 'utf-8');
-        await wRF('written-file.txt',`${value1} \t ${value2}`);
-        const wvalue = await uRF('written-file.txt', 'utf-8');
-        console.log(value1 + "\n\t" + value2 + "\n\t" + wvalue);
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-start()
+const server = http.createServer((request, response) => {
+    const ReadStream = fs.createReadStream('text.txt','utf-8');
+    ReadStream.on('open',()=>{
+        ReadStream.pipe(response);
+    })
+    ReadStream.on('error',()=>response.end(err));
+})
+server.listen(5000);
