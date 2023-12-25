4d27fb33a8491407d449f8bc9d50d993e97fde40
Kelish Rai <raikelish7@gmail.com>
2023-12-25 21:48:08 +0545

    express

--- john smilga node express course/02-express-tutorial/app.js
+++ john smilga node express course/02-express-tutorial/app.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express()
 
-
-
 const port = 5000;
 app.listen(port, ()=>{
     console.log(`Server started at ${port}`);
 })
+
+app.get('/',(req,res)=>{
+    res.send('Hello World')
+})
+
+
